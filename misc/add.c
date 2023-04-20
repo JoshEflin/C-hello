5bb1a3b9404cf99cbd36b90cc92bf1c90f1ca308
ragingpenguin <joshuahuntereflin@gmail.com>
2023-04-19 23:54:40 -0400

    learning printf and scanf

---
+++ misc/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("please enter a number press enter and repeat\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    c=a+b;
+    printf("value of string is %d\n", c);
+    return 0;
+}
