adc4ff4e36ff48e19e19f02cada61dff296faf6b
ragingpenguin <joshuahuntereflin@gmail.com>
2023-04-28 23:37:22 -0400

    working on another basic file

---
+++ UDEMY/add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int  a, b, c;
+    printf("Enter the value of a");
+    scanf("%d", &a);
+    printf("Enter the value of b");
+    scanf("%d", &b);
+    c = a + b;
+    printf("value of c is %d\n", c);
+    return 0;
+}
