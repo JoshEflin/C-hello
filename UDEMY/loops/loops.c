e8c64518d3e50aebc93c006bbf0678eb95ed083c
ragingpenguin <joshuahuntereflin@gmail.com>
2023-08-23 16:47:03 -0400

    new commit

--- UDEMY/loops/loops.c
+++ UDEMY/loops/loops.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int count =1 ;
-
-    while(count  <= 10)
-    {
-        printf("'ello mate");
-        count = count+1; 
+    int a = 1, b = 5;
+    for ( a++ ; a < 5 ; b += 5) {
+        a++;
+        printf("%d \nis the value of a\n",a);
+        printf("%d \nis the value of b\n", b);
     }
+    printf("%d", b);
     return 0;
 }
