c3ceb8ac19d371d0ec32b9add4563ae689aced5f
ragingpenguin <joshuahuntereflin@gmail.com>
2024-01-03 14:46:08 -0500

    algo in C

---
+++ Algorithms/quicksort.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int sort (int[] arr ,int pivot)   
+int main () {
+    int pivot=3;    
+    int[] unsorted = [1,2,5,3,2];    
+    sort(unsorted,pivot);
+    }
